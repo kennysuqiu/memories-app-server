a9b23c7a8077403ac572bdb8ec52f4a8ed5dfb3a
Kenny Su Qiu <kfernandosuqiu@gmail.com>
2022-07-14 16:14:23 -0400

    test

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors);
 //https://www.mongodb.com/cloud.atlas
 const CONNECTION_URL = "mongodb+srv://kennysuqiu:nightsaber97@memories-app-cluster.zuuxwos.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
-
+//test
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
